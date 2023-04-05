cd3dae14f15a18155766ecc782c62abfb47476d8
SmileyFaceImoji <119764534+SmileyFaceImoji@users.noreply.github.com>
2023-04-05 15:32:47 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GRP26Project.Startup))]
+namespace GRP26Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
